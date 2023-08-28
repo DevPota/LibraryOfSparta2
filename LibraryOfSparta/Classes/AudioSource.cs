d732753ce52b33983dfbce2bc55c2d808a3a2761
Pota <haechan9502@gmail.com>
2023-08-29 02:20:27 +0900

    BGM, SFX Added
    BGM, SFX 리소스 추가
    BGM 재생, 일시정지, 재개, 루프, 정지
    SFX 재생, 풀링 추가

--- LibraryOfSparta/Classes/AudioSource.cs
+++ LibraryOfSparta/Classes/AudioSource.cs
@@ -1,12 +1,185 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
+using System.Runtime.InteropServices;
+using System.Timers;
 
 namespace LibraryOfSparta.Classes
 {
-    public class AudioSource
+    class AudioSource
     {
+
+        [DllImport("winmm.dll")]
+        private static extern int mciSendString(String strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+        [DllImport("winmm.dll")]
+        public static extern int mciGetErrorString(int errCode, StringBuilder errMsg, int buflen);
+        [DllImport("winmm.dll")]
+        public static extern int mciGetDeviceID(string lpszDevice);
+
+        public AudioSource()
+        {
+
+        }
+
+        public AudioSource(string filename, string alias)
+        {
+            _medialocation = filename;
+            _alias = alias;
+            LoadMediaFile(_medialocation, _alias);
+        }
+
+        int _deviceid = 0;
+        bool isLooping = false;
+        double currentPlayed = 0.0f;
+        System.Timers.Timer timer = new System.Timers.Timer();
+
+        public int Deviceid
+        {
+            get { return _deviceid; }
+        }
+
+        private bool _isloaded = false;
+
+        public bool Isloaded
+        {
+            get { return _isloaded; }
+            set { _isloaded = value; }
+        }
+
+        private string _medialocation = "";
+
+        public string MediaLocation
+        {
+            get { return _medialocation; }
+            set { _medialocation = value; }
+        }
+        private string _alias = "";
+
+        public string Alias
+        {
+            get { return _alias; }
+            set { _alias = value; }
+        }
+
+        private int _length = 0;
+        public int Length
+        {
+            get { return _length;  }
+            set { _length = value; }
+        }
+
+        public bool LoadMediaFile(string filename, string alias)
+        {
+            _medialocation = filename;
+            _alias = alias;
+            StopPlaying();
+            CloseMediaFile();
+            string Pcommand = "open \"" + filename + "\" alias " + alias;
+            int ret = mciSendString(Pcommand, null, 0, IntPtr.Zero);
+            _isloaded = (ret == 0) ? true : false;
+            if (_isloaded)
+            {
+                _deviceid = mciGetDeviceID(_alias);
+                _length = GetLength();
+            }
+            return _isloaded;
+        }
+
+        public void PlayFromStart()
+        {
+            if (_isloaded)
+            {
+                string Pcommand = "play " + Alias + " from 0";
+                string asd = "setaudio " + Alias + " volume to " + 1000.ToString();
+                int temp = mciSendString(asd, null, 0, IntPtr.Zero);
+                int ret = mciSendString(Pcommand, null, 0, IntPtr.Zero);
+            }
+        }
+
+        public void PlayFromStart(IntPtr callback)
+        {
+            if (_isloaded)
+            {
+                string Pcommand = "play " + Alias + " from 0 notify";
+                int ret = mciSendString(Pcommand, null, 0, callback);
+            }
+        }
+
+        public void PlayLoop()
+        {
+            isLooping = true;
+            PlayFromStart();
+            timer = new System.Timers.Timer(Length);
+
+            timer.Elapsed += new ElapsedEventHandler(Loop);
+            timer.AutoReset = true;
+            timer.Start();
+        }
+
+        public void Loop(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (isLooping == false)
+            {
+                return;
+            }
+            timer.Interval = Length;
+            LoadMediaFile(_medialocation, _alias);
+            PlayFromStart();
+        }
+
+        public void Pause()
+        {
+            mciSendString("pause " + Alias, null, 0, IntPtr.Zero);
+            timer.Interval = int.MaxValue;
+            currentPlayed = Length - GetCurentMilisecond();
+        }
+
+        public void Resume()
+        {
+            mciSendString("resume " + Alias, null, 0, IntPtr.Zero);
+            timer.Interval = currentPlayed;
+        }
+
+        public void CloseMediaFile()
+        {
+            string Pcommand = "close " + Alias;
+            int ret = mciSendString(Pcommand, null, 0, IntPtr.Zero);
+            _isloaded = false;
+
+        }
+
+        public void StopPlaying()
+        {
+            string Pcommand = "stop " + Alias;
+            int ret = mciSendString(Pcommand, null, 0, IntPtr.Zero);
+        }
+
+        public void ForceStopAll()
+        {
+            string Pcommand = "stop " + Alias;
+            int ret = mciSendString(Pcommand, null, 0, IntPtr.Zero);
+            isLooping = false;
+            timer.Elapsed -= Loop;
+            timer.Stop();
+        }
+
+        public int GetLength()
+        {
+            StringBuilder lengthBuf = new StringBuilder(32);
+            string PCommand = "status " + Alias + " length";
+            mciSendString(PCommand, lengthBuf, lengthBuf.Capacity, IntPtr.Zero);
+
+            int length = 0;
+            int.TryParse(lengthBuf.ToString(), out length);
+
+            return length;
+        }
+
+        public int GetCurentMilisecond()
+        {
+            StringBuilder returnData = new StringBuilder(128);
+            string Pcommand = "status "+ Alias + " position";
+            int error = mciSendString(Pcommand, returnData,
+                                  returnData.Capacity, IntPtr.Zero);
+            return int.Parse(returnData.ToString());
+        }
     }
 }
